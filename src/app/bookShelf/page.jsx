e2815d2d6a9d230466bfa3c3cbfd1965572f2f56
MVPRunningMan <2450312334G@Gmail.com>
2023-12-21 17:41:27 +0800

    实现从书架中抽出一本书的效果

--- src/app/bookShelf/page.jsx
+++ src/app/bookShelf/page.jsx
@@ -1,22 +1,36 @@
-'use client'
-
-import styles from './page.module.css'
-
-import { useState } from 'react'
+import styles from "./page.module.css";
 
 const page = () => {
-
-  const list = ["1","2","3","4"]
+  const books = [
+    { color: "blue", name: "斗破苍穹" },
+    { color: "green", name: "武动乾坤" },
+    { color: "orange", name: "大主宰" },
+    { color: "#f1f1f1", name: "元尊" },
+    { color: "red", name: "万相之王" },
+  ];
 
   return (
-    <div class={styles.container}>
-      {
-        list.map((item)=>{
-          return <div>{item}</div>
-        })
-      }
+    <div className={styles.container}>
+      <div className={styles.box}>
+        {books.map((item, index) => (
+          <div
+            key={index}
+            //jsx内使用模板字符串可以实现多个class名
+            className={`${styles.every}`}
+            style={{
+              // 每一个元素比前一个元素的图层大1实现堆叠效果
+              zIndex: index + 1,
+              background: item.color,
+              //每一个元素向右移动60px,实现交错效果
+              left: `${(index + 1) * 60}px`,
+            }}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
