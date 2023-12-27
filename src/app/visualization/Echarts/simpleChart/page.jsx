dbb7ca873a12ae353df8721c3421996e4014bcb5
MVPRunningMan <2450312334G@Gmail.com>
2023-12-27 17:30:39 +0800

    简单的echarts柱状图

---
+++ src/app/visualization/Echarts/simpleChart/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import EChartComponent from './EChartComponent';
+
+const page = () => {
+  return (
+    <div>
+      <h1>My EChart</h1>
+      <EChartComponent />
+    </div>
+  );
+};
+
+export default page;
